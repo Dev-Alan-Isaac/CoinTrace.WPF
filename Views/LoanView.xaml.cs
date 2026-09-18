7e18025e2055f6daf746e8747db47cd0bd844b2c
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2026-09-18 17:58:41 -0600

    ...

---
+++ Views/LoanView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoinTrace.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for LoanView.xaml
+    /// </summary>
+    public partial class LoanView : UserControl
+    {
+        public LoanView()
+        {
+            InitializeComponent();
+        }
+    }
+}
